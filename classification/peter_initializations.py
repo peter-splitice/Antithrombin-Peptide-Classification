87a4db2ce8c21d66adf4c48c8cca4c8e1ea41719
peter-splitice <pvpham92@gmail.com>
2023-05-20 12:51:55 -0700

    5/20 commit 2

--- classification/peter_initializations.py
+++ classification/peter_initializations.py
@@ -1,6 +1,29 @@
 
 import sys
 import logging
+import os
+import pickle
+
+# Basic imports
+import pandas as pd
+import numpy as np
+
+# Preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from data_preprocessing_packages.feature_extraction import extract_feature
+
+# Models
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from xgboost import XGBClassifier
+
+# Global Constants
+PATH = os.getcwd()
+RAND = 42
+FOLDS = 5
 
 # Creating a logger to record and save information.
 def log_files(logname):
@@ -36,3 +59,105 @@ def log_files(logname):
     logger.addHandler(stdout_handler)
 
     return logger
+
+# Data Import + MinMaxScaler transformation
+def import_data():
+    """Import the dataset and apply MinMaxScaler transformation to it first.
+    
+    Parameters
+    ----------
+    None
+    
+    Returns
+    -------
+    x_train_full_mms: 80% of the x-data, before k-fold. MinMax Scaled.
+
+    x_test_mms: 20% of the x-data for our test set. MinMax Scaled.
+
+    y_train_full: 80% of the y-data, before k-fold.
+
+    y_test: 20% of the y-data for our test set.
+
+    """
+
+    # Import dataframes
+    positive = pd.read_csv('data/Positive data.csv')
+    negative = pd.read_csv('data/Negative data.csv')
+
+    # Extract features
+    positive_data = extract_feature(positive['Seq'])
+    negative_data = extract_feature(negative['Seq'])
+
+    # Add targets
+    positive_data['Class'] = 1
+    negative_data['Class'] = 0
+
+    positive_data['SeqLength_bins'] = pd.qcut(positive_data['SeqLength'], q=5)
+    negative_data['SeqLength_bins'] = pd.qcut(negative_data['SeqLength'], q=5)
+
+    full_data = pd.concat([positive_data, negative_data])
+
+    # Split the dataset:
+    x_train_full, x_test, y_train_full, y_test = train_test_split(full_data, full_data['Class'], test_size=0.2, 
+                                                                  stratify=full_data[['SeqLength_bins', 'Class']],
+                                                                  random_state=RAND)
+
+    # Clean the X data by removing seqlength and class
+    x_train_full = x_train_full.drop(columns=['SeqLength_bins', 'Class'])
+    x_test = x_test.drop(columns=['SeqLength_bins', 'Class'])
+
+    ## Apply MinMaxScaler to the data and save.
+    scaler = MinMaxScaler()
+
+    x_train_full_mms = pd.DataFrame(scaler.fit_transform(x_train_full), index=x_train_full.index,
+                                    columns=x_train_full.columns)
+    x_test_mms = pd.DataFrame(scaler.transform(x_test), index=x_test.index, columns=x_test.columns)
+
+    with open(PATH + '/dependency.Scaler.pkl', 'wb') as f:
+        pickle.dump(scaler, f)
+
+    return x_train_full_mms, x_test_mms, y_train_full, y_test
+
+# Create the classification models.
+def classification_models():
+    """
+    Generate the base models with parameters and put them together into a list
+    
+    Parameters
+    ----------
+    None
+    
+    Returns
+    -------
+    clf_models:  Zipped list for our models.  Contains names, hyperparameters, and the models.
+    """
+
+    # Hyperparameters to tune.
+    rbf_params = {'gamma': [1e-1,1e-2,1e-3,1e-4,'scale','auto'], 'C': np.arange(1,101,5),
+                  'class_weight': [None,'balanced'], 'break_ties': [False,True]}
+    lin_params = {}
+    log_params = {}
+    rfc_params = {'criterion': ['gini','entropy'], 'max_features': ['sqrt','log2',1.0,0.3], 'ccp_alpha': np.arange(0,0.3,0.1),
+                  'n_estimators': np.arange(1,25,1), 'max_depth': np.arange(2,11,1)}
+    knn_params = {'n_neighbors': np.arange(1,55,2), 'weights': ['uniform', 'distance'], 'leaf_size': np.arange(5,41,2),
+                  'p': [1, 2], 'keepdims': [False,True]}
+    xgb_params = {'max_depth': np.arange(2,11,1), 'n_estimators': np.arange(1,25,1), 'gamma': np.arange(0,4,1),
+                  'subsample': [0.5,1], 'lambda': [1,5,9], 'alpha': np.arange(0,1.1,0.2)}
+    params = [rbf_params, lin_params, log_params, rfc_params, knn_params, xgb_params]
+
+    # Names of the models for use later.
+    names = ['SVC with RBF Kernel', 'SVC with Linear Kernel', 'Logistic Regression Classifier', 
+             'Random Forest Classifier', 'KNN Classifier', 'XGBoost Classifier']
+    
+    # List of models.
+    rbf_model = SVC(class_weight='balanced')
+    lin_model = SVC(kernel = 'linear', class_weight='balanced')
+    log_model = LogisticRegression(class_weight='balanced', max_iter=500)
+    rfc_model = RandomForestClassifier(class_weight='balanced')
+    knn_model = KNeighborsClassifier()
+    xgb_model = XGBClassifier(scale_pos_weight=8.9038)
+    models = [rbf_model, lin_model, log_model, rfc_model, knn_model, xgb_model]
+
+    clf_zip = list(zip(names, params, models))
+
+    return clf_zip
--- classification/peter_testbench.py
+++ classification/peter_testbench.py
@@ -1,6 +1,4 @@
 ## Imports
-
-# Operating Ssytem
 import os
 import sys
 
@@ -11,81 +9,25 @@ parent_dir = os.path.dirname(current_dir)
 # Add the parent directory to the current path
 sys.path.append(parent_dir)
 
-# Data Analysis
-import pandas as pd
-import matplotlib.pyplot as plt
-import pickle
-
-# Models
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.linear_model import LogisticRegression
-from xgboost import XGBClassifier
-
-# Initializaitons
+# Operating Ssytem
 from peter_initializations import *
+
+# Data Analysis
+import matplotlib.pyplot as plt
 
 # Tuning
 from sklearn.model_selection import GridSearchCV
-
-# Data Processing
-from sklearn.model_selection import train_test_split
-from data_preprocessing_packages.feature_extraction import extract_feature
-from sklearn.preprocessing import MinMaxScaler
-
-# K-Fold Cross Validation
+from sklearn.feature_selection import SequentialFeatureSelector
+from sklearn.decomposition import PCA
+from imblearn.over_sampling import SMOTE
 from sklearn.model_selection import StratifiedKFold
+
 
 # Metrics
 from sklearn.metrics import accuracy_score, make_scorer, matthews_corrcoef
 
-# Global Variables
-PATH = os.getcwd()
-RAND = 42
-
-# Create the classification models.
-def classification_models():
-    """
-    Generate the base models with parameters and put them together into a list
-    
-    Parameters
-    ----------
-    None
-    
-    Returns
-    -------
-    clf_models:  Zipped list for our models.  Contains names, hyperparameters, and the models.
-    """
-
-    # Hyperparameters to tune.
-    rbf_params = {}
-    lin_params = {}
-    log_params = {}
-    rfc_params = {}
-    knn_params = {}
-    xgb_params = {}
-    params = [rbf_params, lin_params, log_params, rfc_params, knn_params, xgb_params]
-
-    # Names of the models for use later.
-    names = ['SVC with RBF Kernel', 'SVC with Linear Kernel', 'Logistic Regression Classifier', 
-             'Random Forest Classifier', 'KNN Classifier', 'XGBoost Classifier']
-    
-    # List of models.
-    rbf_model = SVC(class_weight='balanced')
-    lin_model = SVC(kernel = 'linear', class_weight='balanced')
-    log_model = LogisticRegression(class_weight='balanced', max_iter=500)
-    rfc_model = RandomForestClassifier(class_weight='balanced')
-    knn_model = KNeighborsClassifier()
-    xgb_model = XGBClassifier(scale_pos_weight=8.9038)
-    models = [rbf_model, lin_model, log_model, rfc_model, knn_model, xgb_model]
-
-    clf_zip = list(zip(names, params, models))
-
-    return clf_zip
-
 # Optimization of hyperparameters for regression models using GridSearchCV
-def hyperparameter_optimizer(x, y, params, logger=log_files('clf.log'), model=SVC()):
+def hyperparameter_tuning(x, y, params, step, skf=StratifiedKFold(), model=SVC()):
     """
     This function is responsible for running GridSearchCV and opatimizing our hyperparameters.  I might need to fine-tune this.
 
@@ -104,127 +46,79 @@ def hyperparameter_optimizer(x, y, params, logger=log_files('clf.log'), model=SV
     bestparams: Optimized hyperparameters for the model that we are running the search on.
     """
 
-    logger.debug('GridSearchCV Starting:')
-    logger.debug('-----------------------\n')
-
-    reg = GridSearchCV(model, param_grid=params, scoring=make_scorer(matthews_corrcoef), cv=5, 
-                       return_train_score=True, n_jobs=-1)
-    reg.fit(x,y)
-
-    # Showing the best parameters found on the development set.
-    logger.info('Best parameter set: %s' %(reg.best_params_))
-    logger.info('-------------------------\n')
-
-    # Save the best parameters.
-    bestparams = reg.best_params_
-
-    return bestparams
-
-# Data Import + MinMaxScaler transformation
-def import_data():
-    """Import the dataset and apply MinMaxScaler transformation to it first.
+    # Set up GridSearchCV with KFold Cross Validation
+    clf = GridSearchCV(model, param_grid=params, scoring=make_scorer(matthews_corrcoef), cv=skf,
+                       return_train_score=True, n_jobs=1)
     
-    Parameters
-    ----------
-    None
-    
-    Returns
-    -------
-    x_train_full_mms: 80% of the x-data, before k-fold. MinMax Scaled.
+    logger.info('GridSearchCV Starting:')
+    logger.info('----------------------\n')
 
-    x_test_mms: 20% of the x-data for our test set. MinMax Scaled.
+    clf.fit(x, y)
+    model.set_params(**clf.best_params_)
 
-    y_train_full: 80% of the y-data, before k-fold.
+    logger.info(f'Best parameter set: {clf.best_params_}')
+    logger.info('----------------------------------------\n')
 
-    y_test: 20% of the y-data for our test set.
+    return model
 
+# Sequential Forward Selection stage of the pipeline.
+def sequentail_selection(x, y, name, model=SVC()):
     """
-
-    # Import dataframes
-    positive = pd.read_csv('data/Positive data.csv')
-    negative = pd.read_csv('data/Negative data.csv')
-
-    # Extract features
-    positive_data = extract_feature(positive['Seq'])
-    negative_data = extract_feature(negative['Seq'])
-
-    # Add targets
-    positive_data['Class'] = 1
-    negative_data['Class'] = 0
-
-    positive_data['SeqLength_bins'] = pd.qcut(positive_data['SeqLength'], q=5)
-    negative_data['SeqLength_bins'] = pd.qcut(negative_data['SeqLength'], q=5)
-
-    full_data = pd.concat([positive_data, negative_data])
-
-    # Split the dataset:
-    x_train_full, x_test, y_train_full, y_test = train_test_split(full_data, full_data['Class'], test_size=0.2, 
-                                                                  stratify=full_data[['SeqLength_bins', 'Class']],
-                                                                  random_state=RAND)
-
-    # Clean the X data by removing seqlength and class
-    x_train_full = x_train_full.drop(columns=['SeqLength_bins', 'Class'])
-    x_test = x_test.drop(columns=['SeqLength_bins', 'Class'])
-
-    ## Apply MinMaxScaler to the data and save.
-    scaler = MinMaxScaler()
-
-    x_train_full_mms = pd.DataFrame(scaler.fit_transform(x_train_full), index=x_train_full.index,
-                                    columns=x_train_full.columns)
-    x_test_mms = pd.DataFrame(scaler.transform(x_test), index=x_test.index, columns=x_test.columns)
-
-    with open(PATH + '/dependency.Scaler.pkl', 'wb') as f:
-        pickle.dump(scaler, f)
-
-    return x_train_full_mms, x_test_mms, y_train_full, y_test
-# Optimization of hyperparameters for regression models using GridSearchCV
-def hyperparameter_optimizer(x, y, params, model=SVC()):
-    """
-    This function is responsible for running GridSearchCV and opatimizing our hyperparameters.  I might need to fine-tune this.
+    Perform Sequential Selection on the given dataset, but for the classifer portion of the 
+        model.  MCC is the scorer used.  We perform both forward and backward selection.
 
     Parameters
     ----------
-    x: Input values to perform GridSearchCV with.
+    x: Input values of the dataset.
 
-    y: Output values to create GridSearchCV with.
+    y: Output values for the different classes of the dataset.
 
-    params: Dictionary of parameters to run GridSearchCV on.
-
-    model: The model that we are using for GridSearchCV
+    model: Model function used for Sequential Feature Selection.
 
     Returns
     -------
-    bestparams: Optimized hyperparameters for the model that we are running the search on.
-    """
+    final_x_sfs: Input values of the dataset with the proper number of features selected.
 
-    logger.debug('GridSearchCV Starting:')
-    logger.debug('-----------------------\n')
+    final_sfs: The SequentialFeatureSelector model selected
+    """  
 
-    reg = GridSearchCV(model, param_grid=params, scoring=make_scorer(matthews_corrcoef), cv=5, 
-                       return_train_score=True, n_jobs=-1)
-    reg.fit(x,y)
+    logger.info('Forward Selection Starting')
+    logger.info('--------------------------\n')
 
-    # Showing the best parameters found on the development set.
-    logger.info('Best parameter set: %s' %(reg.best_params_))
-    logger.info('-------------------------\n')
+    # Set the ratios for the number of features we want to extract from forward selection.
+    ratios = np.arange(0.05, 0.55, 0.01)
 
-    # Save the best parameters.
-    bestparams = reg.best_params_
-
-    return bestparams
-
+    for ratio in ratios:
+        sfs = SequentialFeatureSelector(model, n_jobs=-1, scoring=make_scorer(matthews_corrcoef),
+                                        cv=skf, n_features_to_select=ratio, direction='forward')
+        
+        sfs.fit(x,y)
+        x_sfs=pd.DataFrame(sfs.transform(x), columns=sfs.get_feature_names_out())
 
 logger = log_files('clf.log')
-
 
 # Step 1:  Import the data and models
 x_train_full, x_test, y_train_full, y_test = import_data()
 clf_zip = classification_models()
 
+# Instantiate our StratifiedKFold object outside of our loop to pass into each stage.
+skf = StratifiedKFold(n_splits=FOLDS, shuffle=True, random_state=RAND)
+
 for name, params, model in clf_zip:
     print(f'Model:{name}')
 
-    skf = StratifiedKFold(n_splits=5, shuffle=False, random_state=RAND)
+    # Step 2: Perform Hyperparameter Tuning and record the results
+    
+    model = hyperparameter_tuning(x_train_full, y_train_full, params, step='baseline', skf=skf, model=model)
+
+    # Step 3: Perform Sequential Forward Selection on our model (for now we use brute force)
+
+    print('test\n')
+
+    
+#    for train_index, valid_index in skf.split(x_train_full, y_train_full):
+#        x_train, x_valid = x_train_full.iloc[train_index], x_train_full.iloc[valid_index]
+#        y_train, y_valid = y_train_full.iloc[train_index], y_train_full.iloc[valid_index]
 
 
 # Step 2: Apply Hyperparameter Tuning on all of our models.
